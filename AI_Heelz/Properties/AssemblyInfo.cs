3e90891b9b0edfa6adf422003fa8be0d1bfd62ca
hooh_hooah <hooh_hooah@protonmail.com>
2020-06-16 23:33:44 +0900

    Updated AssemblyInfo.cs

--- AI_Heelz/Properties/AssemblyInfo.cs
+++ AI_Heelz/Properties/AssemblyInfo.cs
@@ -6,13 +6,10 @@ using Heelz;
 [assembly: AssemblyDescription("High heels plugin for AI-Shoujo")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/hooh-hooah/AI_Heelz")]
-[assembly: AssemblyProduct("Heelz")]
+[assembly: AssemblyProduct("AI Heelz")]
 [assembly: AssemblyCopyright("Copyright © 2019 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("95b23a3f-13e0-4d09-841e-bc16ce1dc897")]
-
 [assembly: AssemblyVersion(AIHeelz.VERSION)]
